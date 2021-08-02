b2d50f835ffdab427c53fbf481cf540b148bae5c
Raja-mishra1 <rajamishra0702@gmail.com>
2021-08-02 22:45:42 +0530

    added details abt mdoel description

--- src/backend/subscribe_letter/models.py
+++ src/backend/subscribe_letter/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Newsletter(models.Model):
+    """[Model to store newsletter table details]
+
+    Args:
+        models ([type]): [django models]
+
+    Returns:
+        [str]: [firstname of user]
+    """
     firstname = models.CharField(max_length=30)
     email = models.EmailField()
     agree = models.BooleanField()
