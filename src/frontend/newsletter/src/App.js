cc412f059d59dbb4a031b3811aadc6b5173d331e
Raja-mishra1 <rajamishra0702@gmail.com>
2021-08-03 15:09:27 +0530

    returned axios response

--- src/frontend/newsletter/src/App.js
+++ src/frontend/newsletter/src/App.js
@@ -38,7 +38,9 @@ const App = () => {
                     'http://127.0.0.1:8000/api/v1/email-signup/',
                     body,
                     config
-                );
+                ).then(function(response) {
+                    console.log(response.data);
+                });
             } catch(err) {
 
             }
